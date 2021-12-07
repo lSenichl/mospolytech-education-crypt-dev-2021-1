0bbbc6003489c520f4f0bec5c2eb320d7f445257
lSenichl <baryshnikov.semen@mail.ru>
2021-12-07 17:56:33 +0300

    ...

--- lab11_28_dh.py
+++ lab11_28_dh.py
@@ -1,18 +1,8 @@
-def cipher(a, n, ka, kb):
-    a = int(a)
-    n = int(n)
-    ka = int(ka)
-    Ya = a**ka % n
-    print("Ваш Ya = ", Ya)
-    kb = int(kb)
-    Yb = a**kb % n
-    print("Ваш Yb = ", Yb)
-    Ka = (Yb**ka) % n
-    Kb = (Ya**kb) % n
-    print("Первый получившийся ключ = ", Ka)
-    print("Второй получившийся ключ = ", Kb)
-    if Ka == Kb:
-        K = Ka
-        print("Ваш общий ключ", K)
-    else:
-        print("Ключи не сошлись")
+a = int(input("Введите число а"))
+n = int(input("Введите число n, n должно быть больше a"))
+ka = int(input("Введите число ka"))
+Ya = a**ka % n
+print ("Ваш Ya = ", Ya)
+Yb = int(input("Введите число Yb, которое прислал собеседник"))
+K = (a**(Ya*Yb))%n
+print ("Ваш общий ключ", K)
