b9c56d7efc08f80deb80722753c19db2715459de
lSenichl <baryshnikov.semen@mail.ru>
2021-12-22 21:04:45 +0300

    fix lab03

--- program_interface/app/lab03.py
+++ program_interface/app/lab03.py
@@ -1,77 +1,76 @@
 from flask import Flask, render_template, Blueprint, redirect, url_for, flash, request
-from base import input_for_cipher_short, input_for_cipher_long, output_from_decrypted, replace_all_to, clear_text
+from base import input_for_cipher_short, input_for_cipher_long, output_from_decrypted, replace_all_to, clear_text, alphabet
 from base import dict as dictionary
 import numpy as np
 from egcd import egcd
 
 bp = Blueprint('lab03', __name__, url_prefix='/lab03')
 
-alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя "
-
-letter_to_index = dict(zip(alphabet, range(len(alphabet))))
-index_to_letter = dict(zip(range(len(alphabet)), alphabet))
-
-
-def matrix_mod_inv(matrix, modulus):
-    det = int(np.round(np.linalg.det(matrix)))
-    det_inv = egcd(det, modulus)[1] % modulus
-    matrix_modulus_inv = (
-        det_inv * np.round(det * np.linalg.inv(matrix)).astype(int) % modulus
-    )
-
-    return matrix_modulus_inv
-
-
-def matrix_encode(message, K):
-    encrypted = ""
-    message_in_numbers = []
-    for letter in message:
-        message_in_numbers.append(letter_to_index[letter])
-
-    split_P = [
-        message_in_numbers[i: i + int(K.shape[0])]
-        for i in range(0, len(message_in_numbers), int(K.shape[0]))
-    ]
-
-    for P in split_P:
-        P = np.transpose(np.asarray(P))[:, np.newaxis]
-
-        while P.shape[0] != K.shape[0]:
-            P = np.append(P, letter_to_index[" "])[:, np.newaxis]
-
-        numbers = np.dot(K, P) % len(alphabet)
-        n = numbers.shape[0]
-
-        for idx in range(n):
-            number = int(numbers[idx, 0])
-            encrypted += index_to_letter[number]
-    return encrypted
-
-
-def matrix_decode(cipher, Kinv):
-    decrypted = ""
-    cipher_in_numbers = []
-    for letter in cipher:
-        cipher_in_numbers.append(letter_to_index[letter])
-
-    split_C = [
-        cipher_in_numbers[i: i + int(Kinv.shape[0])]
-        for i in range(0, len(cipher_in_numbers), int(Kinv.shape[0]))
-    ]
-
-    for C in split_C:
-        C = np.transpose(np.asarray(C))[:, np.newaxis]
-        numbers = np.dot(Kinv, C) % len(alphabet)
-        n = numbers.shape[0]
-
-        for idx in range(n):
-            number = int(numbers[idx, 0])
-            decrypted += index_to_letter[number]
-    return decrypted
-
 
 @bp.route('/8', methods=['GET', 'POST'])
 def lab03_8():
+    alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя "
+
+    letter_to_index = dict(zip(alphabet, range(len(alphabet))))
+    index_to_letter = dict(zip(range(len(alphabet)), alphabet))
+
+
+    def matrix_mod_inv(matrix, modulus):
+        det = int(np.round(np.linalg.det(matrix)))
+        det_inv = egcd(det, modulus)[1] % modulus
+        matrix_modulus_inv = (
+            det_inv * np.round(det * np.linalg.inv(matrix)).astype(int) % modulus
+        )
+
+        return matrix_modulus_inv
+
+
+    def matrix_encode(message, K):
+        encrypted = ""
+        message_in_numbers = []
+        for letter in message:
+            message_in_numbers.append(letter_to_index[letter])
+
+        split_P = [
+            message_in_numbers[i: i + int(K.shape[0])]
+            for i in range(0, len(message_in_numbers), int(K.shape[0]))
+        ]
+
+        for P in split_P:
+            P = np.transpose(np.asarray(P))[:, np.newaxis]
+
+            while P.shape[0] != K.shape[0]:
+                P = np.append(P, letter_to_index[" "])[:, np.newaxis]
+
+            numbers = np.dot(K, P) % len(alphabet)
+            n = numbers.shape[0]
+
+            for idx in range(n):
+                number = int(numbers[idx, 0])
+                encrypted += index_to_letter[number]
+        return encrypted
+
+
+    def matrix_decode(cipher, Kinv):
+        decrypted = ""
+        cipher_in_numbers = []
+        for letter in cipher:
+            cipher_in_numbers.append(letter_to_index[letter])
+
+        split_C = [
+            cipher_in_numbers[i: i + int(Kinv.shape[0])]
+            for i in range(0, len(cipher_in_numbers), int(Kinv.shape[0]))
+        ]
+
+        for C in split_C:
+            C = np.transpose(np.asarray(C))[:, np.newaxis]
+            numbers = np.dot(Kinv, C) % len(alphabet)
+            n = numbers.shape[0]
+
+            for idx in range(n):
+                number = int(numbers[idx, 0])
+                decrypted += index_to_letter[number]
+        return decrypted
     clear = ''
     encrypted = ''
     decrypted = ''
@@ -99,136 +98,134 @@ def lab03_8():
         return render_template('lab03_8.html', clear_text=clear_text, key='3 10 20 20 19 17 23 78 17')
 
 
-alphabet = alphabet.replace(' ', '') + 'abc'
-
-
-def playfair_encode(clearText, key):
-
-    text = clearText
-    new_alphabet = []
-    for i in range(len(key)):
-        new_alphabet.append(key[i])
-    for i in range(len(alphabet)):
-        bool_buff = False
-        for j in range(len(key)):
-            if alphabet[i] == key[j]:
-                bool_buff = True
-                break
-        if bool_buff == False:
-            new_alphabet.append(alphabet[i])
-    mtx_abt_j = []
-    counter = 0
-    for j in range(6):
-        mtx_abt_i = []
-        for i in range(6):
-            mtx_abt_i.append(new_alphabet[counter])
-            counter = counter + 1
-        mtx_abt_j.append(mtx_abt_i)
-    # проверка на одинаковые биграммы
-    for i in range(len(text) - 1):
-        if text[i] == text[i + 1]:
-            if text[i] != 'я':
-                text = text[:i + 1] + 'я' + text[i + 1:]
-            else:
-                text = text[:i + 1] + 'ю' + text[i + 1:]
-    # проверка на четную длину текста
-    if len(text) % 2 == 1:
-        text = text + "я"
-    enc_text = ""
-    for t in range(0, len(text), 2):
-        flag = True
-        for j_1 in range(6):
-            if flag == False:
-                break
-            for i_1 in range(6):
-                if flag == False:
-                    break
-                if mtx_abt_j[j_1][i_1] == text[t]:
-                    for j_2 in range(6):
-                        if flag == False:
-                            break
-                        for i_2 in range(6):
-                            if mtx_abt_j[j_2][i_2] == text[t+1]:
-                                if j_1 != j_2 and i_1 != i_2:
-                                    enc_text = enc_text + \
-                                        mtx_abt_j[j_1][i_2] + \
-                                        mtx_abt_j[j_2][i_1]
-                                elif j_1 == j_2 and i_1 != i_2:
-                                    enc_text = enc_text + \
-                                        mtx_abt_j[j_1][(i_1+1) % 6] + \
-                                        mtx_abt_j[j_2][(i_2+1) % 6]
-                                elif j_1 != j_2 and i_1 == i_2:
-                                    enc_text = enc_text + \
-                                        mtx_abt_j[(j_1+1) % 5][i_1] + \
-                                        mtx_abt_j[(j_2+1) % 5][i_2]
-                                elif j_1 == j_2 and i_1 == i_2:
-                                    enc_text = enc_text + \
-                                        mtx_abt_j[j_1][i_1] + \
-                                        mtx_abt_j[j_1][i_1]
-                                flag = False
-                                break
-    return enc_text
-
-
-def playfair_decode(clearText, key):
-    text = clearText
-    new_alphabet = []
-    for i in range(len(key)):
-        new_alphabet.append(key[i])
-    for i in range(len(alphabet)):
-        bool_buff = False
-        for j in range(len(key)):
-            if alphabet[i] == key[j]:
-                bool_buff = True
-                break
-        if bool_buff == False:
-            new_alphabet.append(alphabet[i])
-    mtx_abt_j = []
-    counter = 0
-    for j in range(6):
-        mtx_abt_i = []
-        for i in range(6):
-            mtx_abt_i.append(new_alphabet[counter])
-            counter = counter + 1
-        mtx_abt_j.append(mtx_abt_i)
-    enc_text = ""
-    for t in range(0, len(text), 2):
-        flag = True
-        for j_1 in range(6):
-            if flag == False:
-                break
-            for i_1 in range(6):
-                if flag == False:
-                    break
-                if mtx_abt_j[j_1][i_1] == text[t]:
-                    for j_2 in range(6):
-                        if flag == False:
-                            break
-                        for i_2 in range(6):
-                            if mtx_abt_j[j_2][i_2] == text[t+1]:
-                                if j_1 != j_2 and i_1 != i_2:
-                                    enc_text = enc_text + \
-                                        mtx_abt_j[j_1][i_2] + \
-                                        mtx_abt_j[j_2][i_1]
-                                elif j_1 == j_2 and i_1 != i_2:
-                                    enc_text = enc_text + \
-                                        mtx_abt_j[j_1][(i_1-1) % 6] + \
-                                        mtx_abt_j[j_2][(i_2-1) % 6]
-                                elif j_1 != j_2 and i_1 == i_2:
-                                    enc_text = enc_text + \
-                                        mtx_abt_j[(j_1-1) % 5][i_1] + \
-                                        mtx_abt_j[(j_2-1) % 5][i_2]
-                                elif j_1 == j_2 and i_1 == i_2:
-                                    enc_text = enc_text + \
-                                        mtx_abt_j[j_1][i_1] + \
-                                        mtx_abt_j[j_1][i_1]
-                                flag = False
-                                break
-    return enc_text
-
-
 @bp.route('/9', methods=['GET', 'POST'])
 def lab03_9():
+    alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя ".replace(' ', '') + 'abc'
+
+
+    def playfair_encode(clearText, key):
+
+        text = clearText
+        new_alphabet = []
+        for i in range(len(key)):
+            new_alphabet.append(key[i])
+        for i in range(len(alphabet)):
+            bool_buff = False
+            for j in range(len(key)):
+                if alphabet[i] == key[j]:
+                    bool_buff = True
+                    break
+            if bool_buff == False:
+                new_alphabet.append(alphabet[i])
+        mtx_abt_j = []
+        counter = 0
+        for j in range(6):
+            mtx_abt_i = []
+            for i in range(6):
+                mtx_abt_i.append(new_alphabet[counter])
+                counter = counter + 1
+            mtx_abt_j.append(mtx_abt_i)
+        # проверка на одинаковые биграммы
+        for i in range(len(text) - 1):
+            if text[i] == text[i + 1]:
+                if text[i] != 'я':
+                    text = text[:i + 1] + 'я' + text[i + 1:]
+                else:
+                    text = text[:i + 1] + 'ю' + text[i + 1:]
+        # проверка на четную длину текста
+        if len(text) % 2 == 1:
+            text = text + "я"
+        enc_text = ""
+        for t in range(0, len(text), 2):
+            flag = True
+            for j_1 in range(6):
+                if flag == False:
+                    break
+                for i_1 in range(6):
+                    if flag == False:
+                        break
+                    if mtx_abt_j[j_1][i_1] == text[t]:
+                        for j_2 in range(6):
+                            if flag == False:
+                                break
+                            for i_2 in range(6):
+                                if mtx_abt_j[j_2][i_2] == text[t+1]:
+                                    if j_1 != j_2 and i_1 != i_2:
+                                        enc_text = enc_text + \
+                                            mtx_abt_j[j_1][i_2] + \
+                                            mtx_abt_j[j_2][i_1]
+                                    elif j_1 == j_2 and i_1 != i_2:
+                                        enc_text = enc_text + \
+                                            mtx_abt_j[j_1][(i_1+1) % 6] + \
+                                            mtx_abt_j[j_2][(i_2+1) % 6]
+                                    elif j_1 != j_2 and i_1 == i_2:
+                                        enc_text = enc_text + \
+                                            mtx_abt_j[(j_1+1) % 5][i_1] + \
+                                            mtx_abt_j[(j_2+1) % 5][i_2]
+                                    elif j_1 == j_2 and i_1 == i_2:
+                                        enc_text = enc_text + \
+                                            mtx_abt_j[j_1][i_1] + \
+                                            mtx_abt_j[j_1][i_1]
+                                    flag = False
+                                    break
+        return enc_text
+
+
+    def playfair_decode(clearText, key):
+        text = clearText
+        new_alphabet = []
+        for i in range(len(key)):
+            new_alphabet.append(key[i])
+        for i in range(len(alphabet)):
+            bool_buff = False
+            for j in range(len(key)):
+                if alphabet[i] == key[j]:
+                    bool_buff = True
+                    break
+            if bool_buff == False:
+                new_alphabet.append(alphabet[i])
+        mtx_abt_j = []
+        counter = 0
+        for j in range(6):
+            mtx_abt_i = []
+            for i in range(6):
+                mtx_abt_i.append(new_alphabet[counter])
+                counter = counter + 1
+            mtx_abt_j.append(mtx_abt_i)
+        enc_text = ""
+        for t in range(0, len(text), 2):
+            flag = True
+            for j_1 in range(6):
+                if flag == False:
+                    break
+                for i_1 in range(6):
+                    if flag == False:
+                        break
+                    if mtx_abt_j[j_1][i_1] == text[t]:
+                        for j_2 in range(6):
+                            if flag == False:
+                                break
+                            for i_2 in range(6):
+                                if mtx_abt_j[j_2][i_2] == text[t+1]:
+                                    if j_1 != j_2 and i_1 != i_2:
+                                        enc_text = enc_text + \
+                                            mtx_abt_j[j_1][i_2] + \
+                                            mtx_abt_j[j_2][i_1]
+                                    elif j_1 == j_2 and i_1 != i_2:
+                                        enc_text = enc_text + \
+                                            mtx_abt_j[j_1][(i_1-1) % 6] + \
+                                            mtx_abt_j[j_2][(i_2-1) % 6]
+                                    elif j_1 != j_2 and i_1 == i_2:
+                                        enc_text = enc_text + \
+                                            mtx_abt_j[(j_1-1) % 5][i_1] + \
+                                            mtx_abt_j[(j_2-1) % 5][i_2]
+                                    elif j_1 == j_2 and i_1 == i_2:
+                                        enc_text = enc_text + \
+                                            mtx_abt_j[j_1][i_1] + \
+                                            mtx_abt_j[j_1][i_1]
+                                    flag = False
+                                    break
+        return enc_text
     clear = ''
     encrypted = ''
     decrypted = ''
